b486792e434b6c2bdede19aa6a503bc562941ec2
Brunohermans <b.hermans@student.tudelft.nl>
2021-02-11 00:15:55 +0100

    fixed small issue

--- website_bruno/src/views/App.vue
+++ website_bruno/src/views/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
     <Header></Header>
+    <div class="row breath_space1"></div>
     <div class="row">
       <div id="content1" class="content col-md-1 col-lg-2"></div>
       <div id="content2" class="content col-md-10 col-lg-8"><router-view/></div>
       <div id="content3" class="content col-md-10 col-lg-8"></div>
     </div>
+    <div class="row breath_space2"></div>
     <Footer></Footer>
   </div>
 </template>
@@ -30,5 +32,13 @@ export default {
 
 #content2 {
   height: 80em;
+  background-color: yellow;
+}
+.breath_space1 {
+  height: 5em;
+}
+
+.breath_space2 {
+  height: 2.5em;
 }
 </style>
