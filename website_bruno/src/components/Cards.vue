a7d5a7f239b029527884d1226d2f2adcc17e43e4
Brunohermans <b.hermans@student.tudelft.nl>
2021-02-15 00:57:39 +0100

    Made the cards work!

---
+++ website_bruno/src/components/Cards.vue
@@ -0,0 +1,36 @@
+<template lang="html">
+  <b-card-group deck>
+    <div v-for="(item, index) in items" v-bind:key="index">
+      <b-card
+         v-bind="{ 'img-src': item.url }"
+         tag="article"
+         style="max-width: 25rem;"
+         class="mb-2">
+
+         <b-card-title> {{ item.name }}  </b-card-title>
+         <b-card-text> {{ item.about }}  </b-card-text>
+         <b-button href="#" variant="primary"> {{ item.button }}</b-button>
+       </b-card>
+    </div>
+  </b-card-group>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      items: [
+        {'id':1, 'name': 'Projects', 'button': 'Find projects', 'url': 'https://picsum.photos/600/300/?image=25',
+        'about': 'Read more about some (coding) projects I have done in the past!'},
+        {'id':2, 'name': 'About me', 'button': 'Get to now me', 'url': 'https://picsum.photos/600/300/?image=25',
+        'about': 'Read more about me'},
+        {'id':3, 'name': 'Contact', 'button': 'Contact me', 'url': 'https://picsum.photos/600/300/?image=25',
+        'about': 'Get to know how to contact me!'}
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
--- website_bruno/src/views/Home.vue
+++ website_bruno/src/views/Home.vue
@@ -2,28 +2,7 @@
   <div id="home">
     <Banner></Banner>
     <div id="projects"  align="center">
-      <b-card-group deck>
-      <b-card
-          title= "test"
-          img-src="https://picsum.photos/600/300/?image=25"
-          img-alt="Image"
-          img-top
-          tag="article"
-          style="max-width: 20rem;"
-          class="mb-2">
-          <b-card-text>
-            Read more about some (coding) projects I have done in the past!
-          </b-card-text>
-
-          <b-button href="#" variant="primary">Go somewhere</b-button>
-        </b-card>
-      </b-card-group>
-      <!-- <b-card-group deck>
-        <Card0></Card0>
-        <Card1></Card1>
-        <Card2></Card2>
-
-      </b-card-group> -->
+      <Cards></Cards>
     </div>
 
   </div>
@@ -32,27 +11,16 @@
 
 <script>
 import Banner from '../components/Banner.vue'
-// import Card0 from '../components/Card0.vue'
-// import Card1 from '../components/Card1.vue'
-// import Card2 from '../components/Card2.vue'
+import Cards from '../components/Cards.vue'
+
 
 export default {
   name: 'Home',
   components: {
     Banner,
-    // Card0,
-    // Card1,
-    // Card2
+    Cards
   },
-  data: function() {
-    return {
-      items: [
-        {'id':1, 'name': 'Projects', 'about': 'Read more about some (coding) projects I have done in the past!'},
-        {'id':2, 'name': 'About me', 'about': 'Read more about me'},
-        {'id':3, 'name': 'Contact', 'about': 'Get to know how to contact me!'},
-      ]
-    }
-  }
+  
 }
 </script>
 
