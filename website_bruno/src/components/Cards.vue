51c44adef92dc6dec3144718af63f8f7e0c57284
Brunohermans <b.hermans@student.tudelft.nl>
2021-02-15 22:18:10 +0100

    fixed cards

--- website_bruno/src/components/Cards.vue
+++ website_bruno/src/components/Cards.vue
@@ -1,11 +1,11 @@
 <template lang="html">
-  <b-card-group deck>
+  <b-card-group columns>
     <div v-for="(item, index) in items" v-bind:key="index">
       <b-card
          v-bind="{ 'img-src': item.url }"
          tag="article"
-         style="max-width: 25rem;"
-         class="mb-2">
+         style="max-width: 25rem; height: 25em;"
+         >
 
          <b-card-title> {{ item.name }}  </b-card-title>
          <b-card-text> {{ item.about }}  </b-card-text>
@@ -20,12 +20,12 @@ export default {
   data: function() {
     return {
       items: [
-        {'id':1, 'name': 'Projects', 'button': 'Find projects', 'url': 'https://picsum.photos/600/300/?image=25',
-        'about': 'Read more about some (coding) projects I have done in the past!'},
+        {'id':1, 'name': 'Projects', 'button': 'Find projects', 'url': 'https://picsum.photos/600/300/?image=1',
+        'about': 'Read more about some projects I have done in the past!'},
         {'id':2, 'name': 'About me', 'button': 'Get to now me', 'url': 'https://picsum.photos/600/300/?image=25',
         'about': 'Read more about me'},
-        {'id':3, 'name': 'Contact', 'button': 'Contact me', 'url': 'https://picsum.photos/600/300/?image=25',
-        'about': 'Get to know how to contact me!'}
+        {'id':3, 'name': 'Contact', 'button': 'Contact me', 'url': 'https://picsum.photos/600/300/?image=24',
+        'about': 'Get to know how to contact me!'},
       ]
     }
   }
